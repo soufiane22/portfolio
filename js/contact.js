999d35ae9ba6f642b9dd281320360d22559617df
Soufiane ALOUI EL IDRISSI <tanger.soufian2015@gmail.com>
2024-02-21 17:24:50 +0100

    Send contact information to the email

--- js/contact.js
+++ js/contact.js
@@ -1,85 +1,61 @@
-// function sendMessage() {
 
-//     console.log('submit data')
-
-//     $.ajax({
-//         url: 'https://api.apispreadsheets.com/data/y7f0lGmpkDgKmaso/',
-//         type: 'post',
-//         data: $("#myForm").serializeArray(),
-//         success: function () {
-//             alert("Email Submitted Thanks You "+"😀")
-//          //   $('#emailInput').val(" ")
-//         },
-//         error: function () {
-//             alert("There was an error :(")
-//         }
-//     });
-
-//     event. preventDefault();
-
-
-//     //'https://api.apispreadsheets.com/data/DB1l2fkC8pt0NBb6/'
-//     //https://www.apispreadsheets.com/table/y7f0lGmpkDgKmaso/
-//     //y7f0lGmpkDgKmaso
-// }
-
-// $(document).ready(function() {
-//     $("#name, #email, #message").on('input', function() {
-//         $(this).siblings('.validity-message').text(""); // Hide error message
-//     });
+$(document).ready(function() {
+    $("#name, #email, #message").on('input', function() {
+        $(this).siblings('.validity-message').text(""); // Hide error message
+    });
     
-//     $("#send-message").click(function(e) {
-//         e.preventDefault(); // prevent default form submission
+    $("#send-message").click(function(e) {
+        e.preventDefault(); // prevent default form submission
         
-//         // Get values of inputs
-//         var name = $("#name").val();
-//         var email = $("#email").val();
-//         var message = $("#message").val();
-//         console.log({name: name, email: email, message: message});
-//         if (name.trim() === '') {
-//             $("#nameValidityMessage").text("Please enter your name.");
-//             return;
-//         } else {
-//             $("#nameValidityMessage").text("");
-//         }
+        // Get values of inputs
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+        console.log({name: name, email: email, message: message});
+        if (name.trim() === '') {
+            $("#nameValidityMessage").text("Please enter your name.");
+            return;
+        } else {
+            $("#nameValidityMessage").text("");
+        }
 
-//         if (!validateEmail(email)) {
-//             $("#emailValidityMessage").text("Please enter a valid email address.");
-//             return;
-//         }else {
-//             $("#emailValidityMessage").text("")
-//         }
+        if (!validateEmail(email)) {
+            $("#emailValidityMessage").text("Please enter a valid email address.");
+            return;
+        }else {
+            $("#emailValidityMessage").text("")
+        }
 
-//         if (message.trim() === '') {
-//             $("#messageValidityMessage").text("Please enter your message.");
-//             return;
-//         }else {
-//             $("#messageValidityMessage").text("")
-//         }
+        if (message.trim() === '') {
+            $("#messageValidityMessage").text("Please enter your message.");
+            return;
+        }else {
+            $("#messageValidityMessage").text("")
+        }
         
-//         // Here, you can do whatever you want with the values
-//         // For example, you can send them to a server using AJAX
+        // Here, you can do whatever you want with the values
+        // For example, you can send them to a server using AJAX
         
-//         // Example of sending values to a server using AJAX
-//         $.ajax({
-//             type: "POST",
-//             url: "your_server_url",
-//             data: {
-//                 name: name,
-//                 email: email,
-//                 message: message
-//             },
-//             success: function(response) {
-//                 // Handle success response
-//             },
-//             error: function(xhr, status, error) {
-//                 // Handle error
-//             }
-//         });
-//     });
+        // Example of sending values to a server using AJAX
+        // $.ajax({
+        //     type: "POST",
+        //     url: "https://api.web3forms.com/submit",
+        //     data: {
+        //         name: name,
+        //         email: email,
+        //         message: message
+        //     },
+        //     success: function(response) {
+        //         // Handle success response
+        //     },
+        //     error: function(xhr, status, error) {
+        //         // Handle error
+        //     }
+        // });
+    });
 
-//     function validateEmail(email) {
-//         var re = /\S+@\S+\.\S+/;
-//         return re.test(email);
-//     }
-// });
+    function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+});
--- js/shared.js
+++ js/shared.js
@@ -7,8 +7,8 @@ document.write('<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" i
 document.write('<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"  integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></' + 'script>');
 
 // Bootstrap
-document.write('<script   src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></' + 'script>');
-document.write('<script   src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"   integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></' + 'script>');
+document.write('<script   src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></' + 'script>');
+document.write('<script   src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"   integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></' + 'script>');
 
 document.write('<script   src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.4.5/css/bootstrapvalidator.min.css" ></' + 'script>');
 
